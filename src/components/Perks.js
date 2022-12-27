da3c91301c1291e33d875f60696451e14ac8b521
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2022-12-27 13:42:43 -0300

    progress saving

--- src/components/Perks.js
+++ src/components/Perks.js
@@ -1,15 +1,18 @@
+import { useContext, useState } from "react";
+
 export default function Perks() {
 	const membershipPerksOnLocalStorage = localStorage.getItem("membershipPerks");
     const [membershipPerks, setMembershipPerks] = useState(membershipPerksOnLocalStorage);
 	return (
 		<>
-			{membershipPerks.map((perk) => (
+			{/* {membershipPerks.map((perk) => (
 				<>
 					<a href={perk.link}>
 						<button id={perk.id}>{perk.title}</button>
 					</a>
 				</>
-			))}
+			))} */}
+            <h1> AQUI SERÃO OS PERKS </h1>
 		</>
 	);
 }
